26e8b71cade3695fa2399e7feb3c0df403298cd5
Anatoly <googlsocial97@gmail.com>
2026-04-26 02:02:18 +0700

    add: webhook

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,18 @@ const signals = ['SIGINT', 'SIGTERM'] as const;
 
 const config = useConfig();
 const bot = await useTelegramBot(config);
+const server = config.hostUrl ? Bun.serve({ port: 3000, routes: { '/ihb': () => new Response('Bun!') } }) : null;
+
+bot.start({
+    dropPendingUpdates: true,
+    deleteWebhook: 'on-conflict-with-polling',
+    webhook: config.hostUrl ? { url: config.hostUrl } : undefined,
+});
 
 for (const signal of signals) {
     process.on(signal, async () => {
         console.log(`${signal} => exiting...`);
-        await bot.stop();
+        await Promise.all([bot.stop, server?.stop() || Promise.resolve()]);
         process.exit(0);
     });
 }
-
-bot.start({ dropPendingUpdates: true, deleteWebhook: 'on-conflict-with-polling' });
