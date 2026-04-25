2ba6c2fcdf7dd5da29a09e9ed9e8ef106d8963f6
Anatoly <googlsocial97@gmail.com>
2026-04-25 23:35:40 +0700

    refact: кулдаун и новое поведение у команд

--- src/bot/use-telegram-bot.ts
+++ src/bot/use-telegram-bot.ts
@@ -1,13 +1,50 @@
-import { Bot } from 'gramio';
-import type { Config } from '../types';
-import addMainOrder from './commands/add-main-order';
-import addTips from './commands/add-tips';
+import { Bot, Composer, MediaUpload } from 'gramio';
+import useRandomTips from '../features/random-tips/use-random-tips';
+import useScreenshot from '../features/use-screenshot';
+import type { BotState, Config } from '../types';
+
+const isCooldown = (lastRequest: number, requestCooldown: number) => Date.now() - lastRequest <= requestCooldown;
 
 export default async (config: Config) => {
-    const bot = new Bot(config.botToken).onStart(() => console.log('bot started')).decorate({});
+    const state: BotState = { lastRequest: 0 };
 
-    await addTips(bot);
-    await addMainOrder(bot, config);
+    const tips = useRandomTips();
+    const withState = new Composer({ name: 'state' })
+        .decorate({ state })
+        .guard((ctx) => !isCooldown(ctx.state.lastRequest, config.requestCooldown))
+        .as('scoped');
+
+    const bot = new Bot(config.botToken).onStart(() => console.log('bot started')).extend(withState);
+
+    const addTips = async () => {
+        const composer = new Composer({ name: 'tips' }).extend(withState).command('tips', async (ctx) => {
+            ctx.state.lastRequest = Date.now();
+            await ctx.send(tips.getTipsAsQuote());
+            ctx.delete();
+        });
+
+        bot.extend(composer);
+        console.log('added command "tips"');
+    };
+
+    const addMainOrder = async () => {
+        const screenshot = await useScreenshot(config);
+        const composer = new Composer({ name: 'main_order' }).extend(withState).command('main_order', async (ctx) => {
+            ctx.state.lastRequest = Date.now();
+            await ctx.sendMedia({
+                type: 'photo',
+                photo: MediaUpload.buffer(await screenshot.get()),
+                caption: tips.getTipsAsQuote(),
+            });
+            ctx.delete();
+        });
+
+        bot.extend(composer).onStop(async () => await screenshot.close());
+        console.log('added command "main_order"');
+    };
+
+    await addTips();
+    await addMainOrder();
 
     return bot;
 };
